cbafc412787f1c1b5912cbaa6f026b836aec0879
Johnny He <72545302+Dimentio233@users.noreply.github.com>
2023-09-05 17:02:10 -0700

    now hamburger menu should work
    when you reopen

--- app.js
+++ app.js
@@ -1,29 +1,36 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li')
+    const navLinks = document.querySelectorAll('.nav-links li');
     
-    console.log("here goes navLinks DOM", navLinks);
-
     burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active')
-        console.log("here goes nav DOM", nav);
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = ''
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`
-            }
-        })
-    
-        burger.classList.toggle('toggle')
-    
-    })
+        nav.classList.toggle('nav-active');
+        
+        if (nav.classList.contains('nav-active')) {
+            // Applying animations when menu is active
+            navLinks.forEach((link, index) => {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            });
+        } else {
+            // Removing animations when menu is inactive
+            navLinks.forEach((link) => {
+                link.style.animation = '';
+            });
+        }
 
+        burger.classList.toggle('toggle');
+    });
+
+    // New code to close hamburger menu when menu item is clicked
     navLinks.forEach((link) => {
         link.addEventListener('click', () => {
             nav.classList.remove('nav-active');  // remove the nav-active class
             burger.classList.remove('toggle');  // revert burger to its original state
+
+            // Removing animations when menu is closed through nav link click
+            navLinks.forEach((link) => {
+                link.style.animation = '';
+            });
         });
     });
 }
